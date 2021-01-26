25beea65d5a9a22e0239edebe86a20dffa9449b0
WellingtonVicen <wellvicen@gmail.com>
2021-01-26 17:22:19 -0300

    exercicio 1.2

--- test_luby/Program.cs
+++ test_luby/Program.cs
@@ -4,18 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace test_luby
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tou de Volta Porra !!");
+            
             Console.WriteLine(CalcularFatorial(5));
-
+            Console.WriteLine(CalcularPremio(100, "vip", null));
+            Console.WriteLine(CalcularPremio(100, "basic", 3));
+            Console.WriteLine(CalcularPremio(100, "premium", 1));
+            Console.WriteLine(CalcularPremio(100, "deluxe", 1));
+            Console.WriteLine(CalcularPremio(100, "special", 1));
             Console.ReadLine();
 
         }
+
+        
 
         static long CalcularFatorial(int number)
         {
@@ -27,6 +34,40 @@ namespace test_luby
             {
                 return number * CalcularFatorial(number - 1);
             }
+        }
+
+        static  double CalcularPremio(int number, string mod, double? quantity)
+        {
+           
+            if (quantity == null)
+            {
+                quantity = 1;
+            }
+            if(mod == "basic")
+            {
+                return (double)(number * 1 * quantity);
+            }
+            else if (mod == "vip")
+            {
+                return (double)(number * 1.2 * quantity);
+            }
+            else if (mod == "premium")
+            {
+                return (double)(number * 1.5 * quantity);
+            }
+            else if (mod == "deluxe")
+            {
+                return (double)(number * 1.8 * quantity);
+            }
+            else if (mod == "special")
+            {
+                return (double)(number * 2 * quantity);
+            }
+
+           
+              return number;
+           
+
         }
     }
 }
