031686d1d3d62580291d3a62a17ea57bb23f835c
WellingtonVicen <wellvicen@gmail.com>
2021-01-26 15:48:16 -0300

    exercicio 1.1

---
+++ test_luby/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_luby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tou de Volta Porra !!");
+            Console.WriteLine(CalcularFatorial(5));
+
+            Console.ReadLine();
+
+        }
+
+        static long CalcularFatorial(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * CalcularFatorial(number - 1);
+            }
+        }
+    }
+}
